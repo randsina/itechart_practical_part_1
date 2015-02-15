a303cae5240f2777db31897c72ea61211c0f3083
Roman Vakulchik <roman.vakulchik@gmail.com>
2015-02-15 21:20:31 +0300

    Refactoring the 5th task

--- part_2/5_disappeared.rb
+++ part_2/5_disappeared.rb
@@ -1,6 +1,16 @@
-require 'set'
 def find_missed_numbers(number, array)
-  (Set.new(1..number) - array).to_a
+  i = 1
+  missed_numbers = []
+  array.each_with_index do |n, index|
+    if n != index + i
+      ((index + i)...n).each { |e| missed_numbers << e }
+      i = n - index
+    end
+  end
+  (array[-1] + 1).upto(number) do |number|
+    missed_numbers << number
+  end
+  missed_numbers
 end
 
 puts find_missed_numbers(ARGV.first.to_i, ARGV[1..-1].map(&:to_i))
